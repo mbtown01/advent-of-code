f9f4cd4d05f7498dd009043df9de3aaa1321b8fd
Mike Townsley <mike@madllama.net>
2022-10-14 19:50:49 -0500

    day 5

---
+++ 2020/day5.py
@@ -0,0 +1,25 @@
+import re
+
+with open('2020/day5.txt') as reader:
+    allLines = list(a.strip() for a in reader.readlines())
+
+maxSeatId = 0
+allSeatIds = list()
+for line in allLines:
+    row, col = line[:7], line[7:]
+    rowNumBin = row.replace('F', '0').replace('B', '1')
+    colNumBin = col.replace('L', '0').replace('R', '1')
+    rowNum, colNum = int(rowNumBin, 2), int(colNumBin, 2)
+    seatId = rowNum*8 + colNum
+    maxSeatId = max(maxSeatId, seatId)
+    allSeatIds.append(seatId)
+
+
+print(f"Max seatId = {maxSeatId}")
+
+allSeatIds = sorted(allSeatIds)
+deltas = list(
+    allSeatIds[i+1] - allSeatIds[i] for i in range(len(allSeatIds)-1))
+for i, delta in enumerate(deltas):
+    if delta == 2:
+        print(f"missing seat is {allSeatIds[i]+1}")
