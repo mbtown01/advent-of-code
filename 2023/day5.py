4ffafd1c49ec4a8d5c116becf204789c7b5f8b1b
Mike Townsley <mike@madllama.net>
2023-12-17 20:42:22 -0600

    clean-up

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,26 +1,13 @@
-from collections import defaultdict
-
 
 class Span:
 
-    def __init__(self, start: int, *, length: int = None, end: int = None):
+    def __init__(self, start: int, *, length: int = None):
         self.start = start
-        if (length is None) == (end is None):
-            raise RuntimeError("Must supply only one of length or end to Span")
-        if length is not None:
-            self.end, self.length = start + length - 1, length
-        if end is not None:
-            self.end, self.length = end, end - start + 1
+        self.end = start + length - 1
+        self.length = length
 
     def __repr__(self) -> str:
         return f"[Span ({self.start}-{self.end}) len={self.length}]"
-
-    def __eq__(self, __value: "Span") -> bool:
-        return (self.start == __value.start and
-                self.end == __value.end)
-
-    def __hash__(self) -> int:
-        return hash((self.start, self.end))
 
     @property
     def points(self):
@@ -62,25 +49,25 @@ class SourceDestinationMap:
                                prevMapping: "SourceDestinationMap",
                                keyPointsSet: set):
         result = set(self.getForwardMapping(a) for a in keyPointsSet)
-        pointSet = set(p for s in self.spanMappingDict
+        pointSet = set(self.getForwardMapping(p)
+                       for s in self.spanMappingDict
                        for m in prevMapping.spanMappingDict.values()
                        for p in [s.start-1, *s.points, s.end+1]
                        if m.containsPoint(p))
-        result.update(set(self.getForwardMapping(a) for a in pointSet))
-        return result
+        return result.union(pointSet)
 
 
 class Implementation:
 
     def __init__(self, dataPath: str = '2023/day5_test.txt'):
         with open(dataPath, encoding="utf8") as reader:
-            self.seedList, self.mappingList, self.locationList = [], [], []
+            self.seedList, self.mappingList = [], []
             for line in reader:
                 line = line.strip()
                 if line.startswith('seeds:'):
                     _, numberText = line.split(': ')
-                    self.seedList += list(int(a)
-                                          for a in numberText.split(' '))
+                    self.seedList += list(
+                        int(a) for a in numberText.split(' '))
                 if line.endswith('map:'):
                     name, _ = line.split(' ')
                     srcName, _, destName = name.split('-')
@@ -98,15 +85,13 @@ class Implementation:
                     self.seedList[i], self.seedList[i], self.seedList[i+1])
 
     def part1(self):
-        for seed in self.seedList:
-            self.locationList.append(seed)
+        seedMappingList = list([a, a] for a in self.seedList)
+        for seedLocationPair in seedMappingList:
             for mapping in self.mappingList:
-                self.locationList[-1] = mapping.getForwardMapping(
-                    self.locationList[-1])
+                seedLocationPair[1] = mapping.getForwardMapping(
+                    seedLocationPair[1])
 
-        result = sorted(
-            list(zip(self.seedList, self.locationList)), key=lambda a: a[1])
-        return result[0]
+        return sorted(seedMappingList, key=lambda a: a[1])[0]
 
     def part2(self):
         prevMapping = self.seedMapping
