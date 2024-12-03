16dc8d22a9031c2b7c78727926b8933d0a8f663b
Mike Townsley <mike@madllama.net>
2024-12-03 11:54:56 -0600

    day 1 and 2

--- 2024/day01.py
+++ 2024/day01.py
@@ -5,13 +5,20 @@ from os import path
 class Implementation:
 
     def __init__(self, dataPath: str):
-        pass
+        self.columns = list(), list()
+
+        with open(dataPath, encoding="utf8") as reader:
+            for line in reader:
+                for i, value in enumerate(line.strip().split('   ')):
+                    self.columns[i].append(int(value))
 
     def part1(self):
-        raise NotImplementedError()
+        return sum(abs(a - b) for a, b in zip(
+            sorted(self.columns[0]), sorted(self.columns[1])))
 
     def part2(self):
-        raise NotImplementedError()
+        return sum(a * sum(a == b for b in self.columns[1])
+                   for a in self.columns[0])
 
 
 class TestCase(unittest.TestCase):
@@ -20,19 +27,19 @@ class TestCase(unittest.TestCase):
     def test_part1_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 11)
 
     def test_part1_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 1258579)
 
     def test_part2_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 31)
 
     def test_part2_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 23981443)
--- 2024/day02.py
+++ 2024/day02.py
@@ -5,13 +5,33 @@ from os import path
 class Implementation:
 
     def __init__(self, dataPath: str):
-        pass
+        with open(dataPath, encoding="utf8") as reader:
+            self.reportList = list(
+                list(int(a) for a in line.split(' '))
+                for line in reader)
+
+    def isValid(self, report: list):
+        d1 = list(a-b for a, b in zip(report[:-1], report[1:]))
+        return all(a in (1, 2, 3) for a in d1) or \
+            all(-a in (1, 2, 3) for a in d1)
+
+    def isValidMinusOne(self, report: list, factor: int):
+        d1 = list(a-b for a, b in zip(report[:-1], report[1:]))
+        s1 = next((a, b) for a, b in enumerate(d1)
+                  if factor*b not in (1, 2, 3))
+        return len(s1) >= 1 and (
+            self.isValid(report[:s1[0]] + report[s1[0]+1:]) or
+            self.isValid(report[:s1[0]+1] + report[s1[0]+2:]))
 
     def part1(self):
-        raise NotImplementedError()
+        return sum(self.isValid(report)
+                   for report in self.reportList)
 
     def part2(self):
-        raise NotImplementedError()
+        return sum(self.isValid(report) or
+                   self.isValidMinusOne(report, 1) or
+                   self.isValidMinusOne(report, -1)
+                   for report in self.reportList)
 
 
 class TestCase(unittest.TestCase):
@@ -20,19 +40,20 @@ class TestCase(unittest.TestCase):
     def test_part1_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 2)
 
     def test_part1_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 269)
 
     def test_part2_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 4)
 
     def test_part2_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 337)
+        # 321 is too low
