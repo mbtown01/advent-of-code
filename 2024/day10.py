2355822f08f79e45f6777e70b687958578b3bbcb
Mike Townsley <mike@madllama.net>
2024-12-16 20:31:57 -0600

    day 11

--- 2024/day10.py
+++ 2024/day10.py
@@ -4,15 +4,41 @@ from os import path
 
 class Implementation:
 
+    nextVecList = ((0, 1), (-1, 0), (0, -1), (1, 0))
+
     def __init__(self, dataPath: str):
         with open(dataPath, encoding="utf8") as reader:
-            pass
+            self.gridMap = {(j, i): int(c)
+                            for j, line in enumerate(reader)
+                            for i, c in enumerate(line.strip())}
+            self.extents = max(self.gridMap.keys())
+
+    def dumpGrid(self, grid: dict):
+        for j in range(self.extents[0]+1):
+            print(''.join(grid.get((j, i)) for i in range(self.extents[1]+1)))
+
+    def countTrails(self, currLoc: tuple, lastVal: int, found: dict):
+        currVal = self.gridMap.get(currLoc)
+        if currVal is None or currVal != lastVal+1:
+            return 0
+        if currVal == 9 and found is None:
+            return 1
+        if currVal == 9 and currLoc not in found:
+            found[currLoc] = 1
+            return 1
+
+        return sum(
+            self.countTrails(
+                (currLoc[0]+vec[0], currLoc[1]+vec[1]), currVal, found)
+            for vec in self.nextVecList)
 
     def part1(self):
-        raise NotImplementedError()
+        return sum(self.countTrails(head, -1, dict())
+                   for head, val in self.gridMap.items() if val == 0)
 
     def part2(self):
-        raise NotImplementedError()
+        return sum(self.countTrails(head, -1, None)
+                   for head, val in self.gridMap.items() if val == 0)
 
 
 class TestCase(unittest.TestCase):
@@ -22,19 +48,19 @@ class TestCase(unittest.TestCase):
     def test_part1_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 36)
 
     def test_part1_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 786)
 
     def test_part2_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 81)
 
     def test_part2_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 1722)
--- 2024/day11.py
+++ 2024/day11.py
@@ -6,13 +6,35 @@ class Implementation:
 
     def __init__(self, dataPath: str):
         with open(dataPath, encoding="utf8") as reader:
-            pass
+            self.stones = reader.readline().strip().split(' ')
+
+    def blink(self, stone: str, level: int, cache: dict):
+        if level == 0:
+            return 1
+        cacheKey = (stone, level)
+        result = cache.get(cacheKey)
+        if result is not None:
+            return result
+
+        if stone == '0':
+            result = self.blink('1', level-1, cache)
+        elif len(stone) % 2 == 0:
+            result = \
+                self.blink(stone[:len(stone)//2], level-1, cache) + \
+                self.blink(str(int(stone[len(stone)//2:])), level-1, cache)
+        else:
+            result = self.blink(str(int(stone)*2024), level-1, cache)
+
+        cache[cacheKey] = result
+        return result
 
     def part1(self):
-        raise NotImplementedError()
+        cache = dict()
+        return sum(self.blink(a, 25, cache) for a in self.stones)
 
     def part2(self):
-        raise NotImplementedError()
+        cache = dict()
+        return sum(self.blink(a, 75, cache) for a in self.stones)
 
 
 class TestCase(unittest.TestCase):
@@ -22,19 +44,14 @@ class TestCase(unittest.TestCase):
     def test_part1_ex(self):
         impl = Implementation(f'{self._pathPrefix}_example.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 55312)
 
     def test_part1_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part1()
-        self.assertEqual(result, 0)
-
-    def test_part2_ex(self):
-        impl = Implementation(f'{self._pathPrefix}_example.txt')
-        result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 182081)
 
     def test_part2_real(self):
         impl = Implementation(f'{self._pathPrefix}_real.txt')
         result = impl.part2()
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 216318908621637)
